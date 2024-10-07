24702a4fa26013c3eb4faea6a15e190eb66b3f7f
tinchodeluca <martindeluca@frba.utn.edu.ar>
2024-10-07 12:06:51 -0300

    3 conditions

--- app/section 5/functions.py
+++ app/section 5/functions.py
@@ -13,3 +13,10 @@ def foo(text):
     if 8 <= len(text):
         return (True)
     return(False)
+
+def foo(temp):
+    if 25 < temp: #if its higher than 25
+        return 'Hot'
+    elif 15 <= temp: #It will include up to 25
+        return 'Warm'
+    return 'Cold' #Other cases
