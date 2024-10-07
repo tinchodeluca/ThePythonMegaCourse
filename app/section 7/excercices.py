5f2c336b011855a69a9d9d2ca18da9a3b85e2fb3
tinchodeluca <martindeluca@frba.utn.edu.ar>
2024-10-07 12:50:02 -0300

    loop replace

--- app/section 7/excercices.py
+++ app/section 7/excercices.py
@@ -21,10 +21,16 @@ for item in colors:
     if isinstance(item, int) and 50 < item:
         print(item)
 
-        
+
 agendadict = {"John Smith"    : 37682929928,
               "Marry Simpons" : 423998200919
             }
             
 for key, value in agendadict.items():
     print(f"{key}: +{value}")
+
+phone_numbers = {"John Smith": "+37682929928", 
+                 "Marry Simpons": "+423998200919"}
+                 
+for key, value in phone_numbers.items():
+    print(value.replace("+","00"))
