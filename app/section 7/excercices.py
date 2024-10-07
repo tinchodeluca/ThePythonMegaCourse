da45537be378ae698f52a2b5c875da169833bb31
tinchodeluca <martindeluca@frba.utn.edu.ar>
2024-10-07 12:47:12 -0300

    loop dict

--- app/section 7/excercices.py
+++ app/section 7/excercices.py
@@ -14,9 +14,17 @@ colors = [11, 34.1, 98.2, 43, 45.1, 54, 54]
 for item in colors:
     if isinstance(item, int):
         print(item)
-        
+
 colors = [11, 34.1, 98.2, 43, 45.1, 54, 54]
 
 for item in colors:
     if isinstance(item, int) and 50 < item:
         print(item)
+
+        
+agendadict = {"John Smith"    : 37682929928,
+              "Marry Simpons" : 423998200919
+            }
+            
+for key, value in agendadict.items():
+    print(f"{key}: +{value}")
