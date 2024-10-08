877f60009cf8db801fe26e00c0a701871051650b
tinchodeluca <martindeluca@frba.utn.edu.ar>
2024-10-07 21:19:11 -0300

    file

--- app/section 11/exc.py
+++ app/section 11/exc.py
@@ -7,3 +7,6 @@ def file_process(char, path):
     with open(path) as file:
         strings = file.read()
     return strings.count(char)
+
+with open('file.txt',"w") as file:
+    file.write('snail')
