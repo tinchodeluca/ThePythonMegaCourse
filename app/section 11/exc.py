7f6dad8b6c9159ba192c442e69ee1decd4ac16f9
tinchodeluca <martindeluca@frba.utn.edu.ar>
2024-10-07 21:04:58 -0300

    count char

--- app/section 11/exc.py
+++ app/section 11/exc.py
@@ -2,3 +2,8 @@ with open('bear.txt') as file:
     content = file.read()
 
 print(content[:90])
+
+def file_process(char, path):
+    with open(path) as file:
+        strings = file.read()
+    return strings.count(char)
